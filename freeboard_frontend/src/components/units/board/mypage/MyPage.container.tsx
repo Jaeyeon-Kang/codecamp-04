167e8795fcd1ad7521fa82dfdb229cf75a5ee384
Jaeyeon-Kang <wodus770@gmail.com>
2021-12-08 15:10:41 +0900

    myPage firebase 로그인하기 없앰

--- freeboard_frontend/src/components/units/board/mypage/MyPage.container.tsx
+++ freeboard_frontend/src/components/units/board/mypage/MyPage.container.tsx
@@ -1,41 +1,14 @@
 import MyPagePresenter from "../mypage/MyPage.presenter";
-import { collection, getFirestore, addDoc } from "firebase/firestore/lite";
-import { firebaseApp } from "../../../../../pages/_app";
-import { ChangeEvent, useState } from "react";
-import { withAuth } from "../../../commons/hocs/withAuth";
+
 
 function MyPageContainer() {
-  const user = collection(getFirestore(firebaseApp), "user");
-
-  const [userInfo, setUserInfo] = useState({
-    email: "",
-    name: "",
-    password: "",
-  });
-
-  const onChangeInput = (event: ChangeEvent<HTMLInputElement>) => {
-    setUserInfo({
-      ...userInfo,
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  const onClickSubmit = async () => {
-    if (!/\w+@\w+\.\w+/.test(userInfo.email)) {
-      alert("이메일을 정확히 입력하세요");
-    }
-    await addDoc(user, {
-      ...userInfo,
-    });
-    alert("welcome");
-  };
+  
 
   return (
     <MyPagePresenter
-      onChangeInput={onChangeInput}
-      onClickSubmit={onClickSubmit}
+ 
     />
   );
 }
 
-export default withAuth(MyPageContainer);
+export default;
