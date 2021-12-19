2b9243e3ff140b854cc5647a58d657dd8d4ce449
Jaeyeon-Kang <wodus770@gmail.com>
2021-12-19 19:52:37 +0900

    app.tsx 재정비

--- freeboard_frontend/pages/_app.tsx
+++ freeboard_frontend/pages/_app.tsx
@@ -1,21 +1,30 @@
-import "../styles/globals.css";
-import "antd/dist/antd.css";
+import {} from "graphql-request";
 import {
   ApolloClient,
   ApolloProvider,
   InMemoryCache,
   ApolloLink,
 } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
 import { Global } from "@emotion/react";
+import "antd/dist/antd.css";
+import "../styles/globals.css";
 import { AppProps } from "next/dist/shared/lib/router/router";
 import { globalStyles } from "../src/commons/styles/globalStyles";
-import { createUploadLink } from "apollo-upload-client";
-import { createContext, useState, useEffect } from "react";
-
 import Layout from "../src/components/commons/layout/index";
+import { createUploadLink } from "apollo-upload-client";
+import {
+  createContext,
+  useState,
+  useEffect,
+  Dispatch,
+  SetStateAction,
+} from "react";
+import { initializeApp } from "firebase/app";
+import { getAccessToken } from "../src/commons/libraries/getAccessToken";
+// import * as Sentry from "@sentry/nextjs";
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -31,7 +40,24 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
-export const GlobalContext = createContext(null);
+// export const GlobalContext = createContext(null);
+
+Sentry.init({
+  dsn: "https://4e8712d4bbfe496081ff1865da01facf@o1091880.ingest.sentry.io/6109512",
+});
+
+interface IGlobalContext {
+  accessToken?: string;
+  setMyAccessToken?: Dispatch<SetStateAction<string>>;
+  userInfo?: {
+    name?: string;
+    email?: string;
+    picture?: string;
+  };
+  setMyUserInfo?: Dispatch<SetStateAction<{}>>;
+}
+
+export const GlobalContext = createContext<IGlobalContext>({});
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [myAccessToken, setMyAccessToken] = useState("");
@@ -42,21 +68,51 @@ function MyApp({ Component, pageProps }: AppProps) {
     userInfo: myUserInfo,
     setUserInfo: setMyUserInfo,
   };
+  console.log(myAccessToken);
+
+  useEffect(() => {
+    // const accessToken = localStorage.getItem("accessToken") || "";
+    // if (accessToken) setMyAccessToken(accessToken);
+
+    if (localStorage.getItem("refreshToken")) getAccessToken(setMyAccessToken);
+  }, []);
+
+  const errorLink = onError(({ graphQLErrors, operation, forward }) => {
+    if (graphQLErrors) {
+      for (const err of graphQLErrors) {
+        // 1. 토큰 만료 에러를 케치
+        if (err.extensions?.code === "UNAUTHENTICATED") {
+          // 3. 기존에 실패했던 요청 다시 재요청하기
+          operation.setContext({
+            headers: {
+              ...operation.getContext().headers,
+              authorization: `Bearer ${getAccessToken(setMyAccessToken)}`, // 2. refreshToken으로 accessToken 재발급 받기 => restoreAccessToken
+            },
+          });
+          return forward(operation);
+        }
+      }
+    }
+  });
+
   const uploadLink = createUploadLink({
     uri: "http://backend04.codebootcamp.co.kr/graphql",
-    headers: { authorization: `Bearer ${myAccessToken}` },
+    headers: {
+      authorization: `Bearer ${myAccessToken}`,
+    },
+    credentials: "include",
   });
 
   const client = new ApolloClient({
     uri: "http://backend04.codebootcamp.co.kr/graphql",
-    link: ApolloLink.from([uploadLink as any]),
+    link: ApolloLink.from([errorLink, uploadLink as any]),
     cache: new InMemoryCache(),
   });
 
-  useEffect(() => {
-    const accessToken = localStorage.getItem("accessToken") || "";
-    if (accessToken) setMyAccessToken(accessToken);
-  });
+  // useEffect(() => {
+  //   const accessToken = localStorage.getItem("accessToken") || "";
+  //   if (accessToken) setMyAccessToken(accessToken);
+  // });
 
   return (
     <GlobalContext.Provider value={myValue}>
